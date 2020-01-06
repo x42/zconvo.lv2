d2c7dabe824950b76865f3e1fe1492a576540ca5
Robin Gareus <robin@gareus.org>
2020-01-06 17:04:46 +0100

    Fix state:freePath Windows fallback

--- src/lv2.cc
+++ src/lv2.cc
@@ -445,11 +445,11 @@ save (LV2_Handle                instance,
 		free_path->free_path (free_path->handle, apath);
 	} else
 #endif
-#ifndef _WIN32 // https://github.com/drobilla/lilv/issues/14
 	{
+#ifndef _WIN32 // https://github.com/drobilla/lilv/issues/14
 		free (apath);
-	}
 #endif
+	}
 
 	ZeroConvoLV2::Convolver::IRSettings const& irs (self->clv_online->settings ());
 
@@ -575,11 +575,11 @@ restore (LV2_Handle                  instance,
 			free_path->free_path (free_path->handle, path);
 		} else
 #endif
-#ifndef _WIN32 // https://github.com/drobilla/lilv/issues/14
 		{
+#ifndef _WIN32 // https://github.com/drobilla/lilv/issues/14
 			free (path);
-		}
 #endif
+		}
 	}
 
 	if (!ok) {
